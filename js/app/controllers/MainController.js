151b08c643dd32d716e6ccd87b2b85b4c4202738
agdavid <antonio.g.david@gmail.com>
2016-08-29 11:33:57 -0400

    define properties for MainController and render in view

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,5 +1,11 @@
 function MainController($scope) {
   //define controller properties
+  $scope.customer = {
+    name: 'Antonio',
+    phone: '555-1234',
+    email: 'antonio@email.com'
+  }
+  $scope.year = '2016'
 }
 
 angular
