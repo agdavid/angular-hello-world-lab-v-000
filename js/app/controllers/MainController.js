fbd85c88bc56e98e99a7d89d684c58065c84d1f7
agdavid <antonio.g.david@gmail.com>
2016-08-29 11:28:25 -0400

    create controller 'MainController' and attach to module

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,7 @@
+function MainController($scope) {
+  //define controller properties
+}
+
+angular
+  .module('app') //fetch existing module
+  .controller('MainController', MainController); //attach controller to module
